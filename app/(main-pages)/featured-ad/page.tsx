73cf0ef361508357a1f6f43ef04889a808ef0d0a
Azeem-py <74514285+Azeem-py@users.noreply.github.com>
2024-07-12 18:58:37 +0100

    program api integration

--- app/(main-pages)/featured-ad/page.tsx
+++ app/(main-pages)/featured-ad/page.tsx
@@ -1,114 +1,120 @@
-"use client";
-import { AD_FEE, FEATURED_AD } from "@/constant";
-import React, { useState } from "react";
-import Link from "next/link";
-import { ChevronRight } from "lucide-react";
-import MainDialog from "@/components/ui/FormField/MainDialog";
-import NestedDialog from "@/components/ui/FormField/NestedDialog";
-import FeaturedAdForm from "@/components/forms/featuredAd";
-import { Button } from "@/components/ui/button";
-import { useRouter} from "next/navigation";
-
+'use client'
+import { AD_FEE, FEATURED_AD } from '@/constant'
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { ChevronRight } from 'lucide-react'
+import MainDialog from '@/components/ui/FormField/MainDialog'
+import NestedDialog from '@/components/ui/FormField/NestedDialog'
+import FeaturedAdForm from '@/components/forms/featuredAd'
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
 
 const page = () => {
-  const [isMainDialogOpen, setIsMainDialogOpen] = useState(false);
-  const [isSuccessDialogOpen, setIsSuccessDialogOpen] = useState(false);
+  const [isMainDialogOpen, setIsMainDialogOpen] = useState(false)
+  const [isSuccessDialogOpen, setIsSuccessDialogOpen] = useState(false)
 
-  const router = useRouter();
+  const router = useRouter()
 
   const handleFormSubmit = (values: any) => {
-    console.log('Form Submitted:', values);
+    console.log('Form Submitted:', values)
     // Close main dialog and open success dialog
-    setIsMainDialogOpen(false);
-    setIsSuccessDialogOpen(true);
+    setIsMainDialogOpen(false)
+    setIsSuccessDialogOpen(true)
     // You can also perform navigation or other actions here
-  };
+  }
   return (
-    <section className="max-container padding-container flex flex-col lg:flex-row gap-10 mb-20 w-full mt-28">
-      <div className="w-full lg:w-[65%] lg:padding-container">
-        <h4 className="text-cream-50 bold-32">Feature Ad</h4>
-        <p className="text-cream-20 regular-16 mt-4">
+    <section className='max-container padding-container flex flex-col lg:flex-row gap-10 mb-20 w-full mt-28'>
+      <div className='w-full lg:w-[65%] lg:padding-container'>
+        <h4 className='text-cream-50 bold-32'>Feature Ad</h4>
+        <p className='text-cream-20 regular-16 mt-4'>
           Promote your brand affiliate program on Affiliate farm and attract
           attention of bloggers, content creator, and YouTubers shopping for new
           offers to promote and drive significant credibility and traffic to
           your brand product massively.
         </p>
-        <div className="mt-10 bg-gray-20 padding-container py-10">
-          <p className="text-cream-50 medium-20">
+        <div className='mt-10 bg-gray-20 padding-container py-10'>
+          <p className='text-cream-50 medium-20'>
             Get mention everywhere with our featured ad:
           </p>
           <ul>
             {FEATURED_AD.map((feature) => (
-              <li className="list-disc ml-5 mb-5 text-cream-20 mt-4 ">
-                <span className="text-cream-20 bold-16">{feature.name}</span>
-                <span className="text-cream-20 regular-16">
+              <li className='list-disc ml-5 mb-5 text-cream-20 mt-4 '>
+                <span className='text-cream-20 bold-16'>{feature.name}</span>
+                <span className='text-cream-20 regular-16'>
                   {feature.description}
                 </span>
               </li>
             ))}
           </ul>
-          <p className="text-cream-50 medium-16">
+          <p className='text-cream-50 medium-16'>
             Maximize your brand's visibility, credibility, and reach by been
             mentioned everywhere on the internet through our featured ads space.
           </p>
         </div>
-        <div className="mt-10">
-          <p className="text-cream-50 meium-20">Have question?</p>
-          <p className="mt-5 text-cream-20 regular-16">
+        <div className='mt-10'>
+          <p className='text-cream-50 meium-20'>Have question?</p>
+          <p className='mt-5 text-cream-20 regular-16'>
             Contact us to ask us any question concerning our sponsor program, or
-            feedback for us, You can mail us{" "}
-            <Link href="/" className="text-yellow-50 underline">
+            feedback for us, You can mail us{' '}
+            <Link href='/' className='text-yellow-50 underline'>
               support@affiliatefarm.xyz
             </Link>
           </p>
         </div>
       </div>
-      <div className="bg-black-30 w-full lg:w-[30%] h-fit py-5 padding-container rounded-xl border border-gray-20">
-        <p className="text-cream-50 bold-24">Featured ad fee</p>
-        <p className="text-cream-20 regular-16 mt-4">
+      <div className='bg-black-30 w-full lg:w-[30%] h-fit py-5 padding-container rounded-xl border border-gray-20'>
+        <p className='text-cream-50 bold-24'>Featured ad fee</p>
+        <p className='text-cream-20 regular-16 mt-4'>
           Your brand affiliate program will be displayed throughout our site
           pages for 30 days (a month).
         </p>
-        <div className="padding-container">
-          <p className="text-cream-50 font-[800px] text-[24px] mt-4">
+        <div className='padding-container'>
+          <p className='text-cream-50 font-[800px] text-[24px] mt-4'>
             $24/month
           </p>
-          <p className="text-cream-20 regular-16 mt-4">This plan include</p>
+          <p className='text-cream-20 regular-16 mt-4'>This plan include</p>
           <ul>
             {AD_FEE.map((fee) => (
-              <li className="text-cream-20 flex items-center gap-3 mt-4 regular-16">
-                <ChevronRight className="text-cream-20 size-4" />
+              <li className='text-cream-20 flex items-center gap-3 mt-4 regular-16'>
+                <ChevronRight className='text-cream-20 size-4' />
                 {fee}
               </li>
             ))}
           </ul>
         </div>
-        <div className="mt-5 flexCenter w-full">
-        <MainDialog
-            title="Buy feature ad space"
+        <div className='mt-5 flexCenter w-full'>
+          <MainDialog
+            title='Buy feature ad space'
             description="By opting into our feature ad plan, you'll maximize your brand's affiliate program visibility, credibility, and reach."
             isOpen={isMainDialogOpen}
             onOpenChange={() => setIsMainDialogOpen(false)}
             onClick={() => setIsMainDialogOpen(true)}
-            buttonName="Subscribe now"
+            buttonName='Subscribe now'
           >
             <FeaturedAdForm onSubmit={handleFormSubmit} />
           </MainDialog>
           <NestedDialog
             isOpen={isSuccessDialogOpen}
             onClose={() => setIsSuccessDialogOpen(false)}
-            title="Brand Affiliate program on Campaign now"
-            description="Your brand will start benefitting from massive exposure and a quickly affiliate program spread though our targeted outreach to content creators."
+            title='Brand Affiliate program on Campaign now'
+            description='Your brand will start benefitting from massive exposure and a quickly affiliate program spread though our targeted outreach to content creators.'
           >
-            <Button className="w-full mt-3"  onClick={() => {
-                router.push("/");
-              }}>Go back now</Button>
-            <p className="text-center regular-16 text-cream-50 mt-3">Feature ad activated now.</p>
+            <Button
+              className='w-full mt-3'
+              onClick={() => {
+                router.push('/')
+              }}
+            >
+              Go back now
+            </Button>
+            <p className='text-center regular-16 text-cream-50 mt-3'>
+              Feature ad activated now.
+            </p>
           </NestedDialog>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default page;
+export default page
--- components/forms/submitProgram.tsx
+++ components/forms/submitProgram.tsx
@@ -1,329 +1,336 @@
-"use client";
-import React, { useState } from "react";
-import { useForm, FormProvider } from "react-hook-form";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "../ui/button";
-import TextInput from "../ui/FormField/TextInput";
-import Dropdown from "../ui/FormField/Dropdown";
-import TextareaInput from "../ui/FormField/TextareaInput";
-import { RadioGroupForm } from "../ui/FormField/RadioButton";
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react'
+import { useForm, FormProvider } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button } from '../ui/button'
+import TextInput from '../ui/FormField/TextInput'
+import Dropdown from '../ui/FormField/Dropdown'
+import TextareaInput from '../ui/FormField/TextareaInput'
+import { RadioGroupForm } from '../ui/FormField/RadioButton'
+import Image from 'next/image'
 import {
   AFFLIATE_TYPE,
   COMMISSION_TYPE,
   PAYMENT_METHOD,
   ALL_LEVELS,
   AFFILIATE_CATEGORY,
-} from "@/constant";
-import { useRouter } from "next/navigation";
+} from '@/constant'
+import { useRouter } from 'next/navigation'
+import { useGetCategories } from '@/services/models/hooks/category/hook'
+import { capitalizeFirstLetter } from '@/lib/helpers/formatWord'
 
 const SubmitProgramSchema = z.object({
   publisher_email: z
     .string()
-    .min(1, "Email is required")
-    .email("Incorrect email address"),
-  brand_name: z.string().min(1, "Brand name is required"),
-  payout_fee: z.string().min(1, "Payout fee is required"),
-  cookie_duration: z.string().min(1, "Cookie duration is required"),
-  publisher_name: z.string().min(1, "Publisher name is required"),
-  affiliates: z.string().min(1, "Affiliate type is required"),
-  category: z.string().min(1, "Brand niche is required"),
-  commissions: z.string().min(1, "Commission Type is required"),
-  payments: z.string().min(1, "Payment Method is required"),
-  cookie_expired: z.string().min(1, "Cookie expiration is required"),
-  program_url: z.string().url("Invalid URL"),
-  description: z.string().min(1, "Product description is required"),
-  program_description: z.string().min(1, "Program description is required"),
-  levels: z.string().min(1, "Affiliates Levels are required"),
-  commission_rate: z.string().nonempty("Commission rate is required"),
-  logo: z.string().nonempty("Logo is required"),
-});
+    .min(1, 'Email is required')
+    .email('Incorrect email address'),
+  brand_name: z.string().min(1, 'Brand name is required'),
+  payout_fee: z.string().min(1, 'Payout fee is required'),
+  cookie_duration: z.string().min(1, 'Cookie duration is required'),
+  publisher_name: z.string().min(1, 'Publisher name is required'),
+  affiliates: z.string().min(1, 'Affiliate type is required'),
+  category: z.string().min(1, 'Brand niche is required'),
+  commissions: z.string().min(1, 'Commission Type is required'),
+  payments: z.string().min(1, 'Payment Method is required'),
+  cookie_expired: z.string().min(1, 'Cookie expiration is required'),
+  program_url: z.string().url('Invalid URL'),
+  description: z.string().min(1, 'Product description is required'),
+  program_description: z.string().min(1, 'Program description is required'),
+  levels: z.string().min(1, 'Affiliates Levels are required'),
+  commission_rate: z.string().nonempty('Commission rate is required'),
+  logo: z.string().nonempty('Logo is required'),
+})
 
 const SubmitProgramForm = () => {
-  const router = useRouter();
+  const router = useRouter()
   const methods = useForm({
     resolver: zodResolver(SubmitProgramSchema),
     defaultValues: {
-      brand_name: "",
-      payout_fee: "",
-      publisher_email: "",
-      cookie_duration: "",
-      publisher_name: "",
-      affiliates: "",
-      commissions: "",
-      payments: "",
-      cookie_expired: "",
-      program_url: "",
-      description: "",
-      program_description: "",
-      levels: "",
-      commission_rate: "",
-      logo: "",
-      category: "",
+      brand_name: '',
+      payout_fee: '',
+      publisher_email: '',
+      cookie_duration: '',
+      publisher_name: '',
+      affiliates: '',
+      commissions: '',
+      payments: '',
+      cookie_expired: '',
+      program_url: '',
+      description: '',
+      program_description: '',
+      levels: '',
+      commission_rate: '',
+      logo: '',
+      category: '',
     },
-  });
+  })
 
-  const Affiliates = AFFLIATE_TYPE.map((affiliate) => ({
-    label: affiliate.name,
-    value: affiliate.name,
-  }));
+  const { data, isSuccess } = useGetCategories()
 
-  const Category = AFFILIATE_CATEGORY.map((category) => ({
-    label: category.name,
-    value: category.name,
-  }));
+  const Affiliates = AFFLIATE_TYPE.map((affiliate) => {
+    return {
+      label: affiliate.name,
+      value: affiliate.name,
+    }
+  })
+
+  const Category = isSuccess
+    ? data.map((category) => ({
+        label: `${capitalizeFirstLetter(category.name)} affiliate program`,
+        value: category.code,
+      }))
+    : [{ label: 'Others', value: 'others' }]
 
   const Commissions = COMMISSION_TYPE.map((commission) => ({
     label: commission.name,
     value: commission.name,
-  }));
+  }))
 
   const Levels = ALL_LEVELS.map((level) => ({
     label: level.name,
     value: level.name,
-  }));
+  }))
 
   const Payments = PAYMENT_METHOD.map((payment) => ({
     label: payment.name,
     value: payment.name,
-  }));
+  }))
 
-  const [rate, setRate] = useState<number | string>("");
-  const [currency, setCurrency] = useState<string>("%"); // Default to %
-  const [logo, setLogo] = useState<string | ArrayBuffer | null>(null);
+  const [rate, setRate] = useState<number | string>('')
+  const [currency, setCurrency] = useState<string>('%') // Default to %
+  const [logo, setLogo] = useState<string | ArrayBuffer | null>(null)
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+    const file = event.target.files?.[0]
     if (file) {
-      const reader = new FileReader();
+      const reader = new FileReader()
       reader.onloadend = () => {
-        setLogo(reader.result);
-        methods.setValue("logo", reader.result as string);
-      };
-      reader.readAsDataURL(file);
+        setLogo(reader.result)
+        methods.setValue('logo', reader.result as string)
+      }
+      reader.readAsDataURL(file)
     }
-  };
+  }
 
   const handleSubmit = (values: z.infer<typeof SubmitProgramSchema>) => {
-
     if (!logo) {
-      methods.setError("logo", { type: "manual", message: "Logo is required" });
-      return;
+      methods.setError('logo', { type: 'manual', message: 'Logo is required' })
+      return
     }
-    
+
     const combinedRate =
-      currency === "%" ? `${rate}${currency}` : `${currency}${rate}`;
-    console.log("Form Values:", { ...values, commission_rate: combinedRate });
-    router.push("/preview-submission");
-  };
+      currency === '%' ? `${rate}${currency}` : `${currency}${rate}`
+    console.log('Form Values:', { ...values, commission_rate: combinedRate })
+    router.push('/preview-submission')
+  }
 
   return (
-    <div className="bg-gray-40 p-6 h-fit rounded-lg">
-      <p className="bold-24 text-cream-50 mb-3">
+    <div className='bg-gray-40 p-6 h-fit rounded-lg'>
+      <p className='bold-24 text-cream-50 mb-3'>
         Submit brand affiliate program
       </p>
-      <p className="regular-16 text-cream-20">
+      <p className='regular-16 text-cream-20'>
         Get your brand exposed to massive traffic from Affiliate Farm.
       </p>
       <FormProvider {...methods}>
         <form
           onSubmit={methods.handleSubmit(handleSubmit)}
-          className="mt-10 space-y-6"
+          className='mt-10 space-y-6'
         >
-          <div className="flex flex-col ">
-            <label className="text-white mb-2">Brand Logo</label>
-            <div className="flex items-center">
-              <div className="w-24 h-24 border border-gray-20 rounded-lg overflow-hidden flex items-center justify-center bg-none">
+          <div className='flex flex-col '>
+            <label className='text-white mb-2'>Brand Logo</label>
+            <div className='flex items-center'>
+              <div className='w-24 h-24 border border-gray-20 rounded-lg overflow-hidden flex items-center justify-center bg-none'>
                 {logo ? (
                   <Image
                     src={logo as string}
                     width={20}
                     height={30}
-                    alt="Logo Preview"
-                    className="w-full h-full object-cover"
+                    alt='Logo Preview'
+                    className='w-full h-full object-cover'
                   />
                 ) : (
                   <Image
-                    src="/logo-preview.svg"
+                    src='/logo-preview.svg'
                     width={20}
                     height={30}
-                    alt=""
-                    className="w-full h-full object-cover"
+                    alt=''
+                    className='w-full h-full object-cover'
                   />
                 )}
               </div>
-              <label className="ml-4 bg-transparent border mt-10 border-gray-20 text-cream-50 px-4 py-2 rounded-lg cursor-pointer">
+              <label className='ml-4 bg-transparent border mt-10 border-gray-20 text-cream-50 px-4 py-2 rounded-lg cursor-pointer'>
                 Upload Favicon
                 <input
-                  type="file"
-                  className="hidden"
+                  type='file'
+                  className='hidden'
                   onChange={handleImageUpload}
                 />
               </label>
             </div>
             {methods.formState.errors.logo && (
-              <p className="text-red-500 mt-2 text-sm">
+              <p className='text-red-500 mt-2 text-sm'>
                 {methods.formState.errors.logo.message}
               </p>
             )}
           </div>
-          <div className="flex gap-10 flex-col lg:flex-row">
+          <div className='flex gap-10 flex-col lg:flex-row'>
             <TextInput
               control={methods.control}
-              name="brand_name"
-              label="Brand Name"
-              placeholder="eg. Jasper AI"
+              name='brand_name'
+              label='Brand Name'
+              placeholder='eg. Jasper AI'
             />
             <Dropdown
               control={methods.control}
-              name="category"
-              label="Brand niche"
+              name='category'
+              label='Brand niche'
               options={Category}
-              placeholder="Travel affiliate program"
+              placeholder='Travel affiliate program'
             />
           </div>
-          <div className="flex gap-10 flex-col lg:flex-row">
-            <div className="w-full">
-              <label className="text-cream-50 regular-16">
+          <div className='flex gap-10 flex-col lg:flex-row'>
+            <div className='w-full'>
+              <label className='text-cream-50 regular-16'>
                 Commission Rate
               </label>
-              <div className="flex items-center overflow-hidden mt-2">
+              <div className='flex items-center overflow-hidden mt-2'>
                 <select
-                  className="px-3 py-2 bg-gray-20 text-cream-50 outline-none"
+                  className='px-3 py-2 bg-gray-20 text-cream-50 outline-none'
                   value={currency}
                   onChange={(e) => setCurrency(e.target.value)}
                 >
-                  <option value="%">%</option>
-                  <option value="$">$</option>
-                  <option value="€">€</option>
-                  <option value="£">£</option>
+                  <option value='%'>%</option>
+                  <option value='$'>$</option>
+                  <option value='€'>€</option>
+                  <option value='£'>£</option>
                 </select>
                 <input
-                  type="number"
-                  className="w-full px-3 py-2 bg-transparent border border-gray-20 rounded-md text-cream-50 placeholder-gray-10 outline-none"
-                  placeholder="eg. 25"
+                  type='number'
+                  className='w-full px-3 py-2 bg-transparent border border-gray-20 rounded-md text-cream-50 placeholder-gray-10 outline-none'
+                  placeholder='eg. 25'
                   value={rate}
                   onChange={(e) => {
-                    const value = Number(e.target.value);
+                    const value = Number(e.target.value)
                     if (!isNaN(value) && value > 0) {
-                      setRate(value);
+                      setRate(value)
                       const combinedRate =
-                        currency === "%"
+                        currency === '%'
                           ? `${value}${currency}`
-                          : `${currency}${value}`;
-                      methods.setValue("commission_rate", combinedRate); // Update the form value with combined rate and currency
+                          : `${currency}${value}`
+                      methods.setValue('commission_rate', combinedRate) // Update the form value with combined rate and currency
                     } else {
-                      setRate("");
-                      methods.setValue("commission_rate", ""); // Clear the form value
+                      setRate('')
+                      methods.setValue('commission_rate', '') // Clear the form value
                     }
                   }}
                 />
               </div>
               {methods.formState.errors.commission_rate && (
-                <p className="text-red-500 mt-2 text-sm">
+                <p className='text-red-500 mt-2 text-sm'>
                   {methods.formState.errors.commission_rate.message}
                 </p>
               )}
             </div>
             <Dropdown
               control={methods.control}
-              name="commissions"
-              label="Commission Type"
+              name='commissions'
+              label='Commission Type'
               options={Commissions}
-              placeholder="Commission Type"
+              placeholder='Commission Type'
             />
           </div>
-          <div className="flex gap-10 flex-col lg:flex-row">
+          <div className='flex gap-10 flex-col lg:flex-row'>
             <TextInput
               control={methods.control}
-              name="payout_fee"
-              label="Payout fee"
-              placeholder="eg $50"
+              name='payout_fee'
+              label='Payout fee'
+              placeholder='eg $50'
             />
             <Dropdown
               control={methods.control}
-              name="payments"
-              label="Payment Method"
+              name='payments'
+              label='Payment Method'
               options={Payments}
-              placeholder="Payment Method"
+              placeholder='Payment Method'
             />
           </div>
           <div>
             <RadioGroupForm
               control={methods.control}
-              label="Cookie expired?"
-              name="cookie_expired"
+              label='Cookie expired?'
+              name='cookie_expired'
             />
           </div>
-          <div className="lg:w-[40%] flex-col lg:flex-row">
+          <div className='lg:w-[40%] flex-col lg:flex-row'>
             <TextInput
               control={methods.control}
-              name="cookie_duration"
-              label="Cookie duration"
-              placeholder="eg. 60 days"
+              name='cookie_duration'
+              label='Cookie duration'
+              placeholder='eg. 60 days'
             />
           </div>
-          <div className="flex gap-10 flex-col lg:flex-row">
+          <div className='flex gap-10 flex-col lg:flex-row'>
             <TextInput
               control={methods.control}
-              name="program_url"
-              label="Affiliate program URL"
-              placeholder="eg. jasper-ai/affiliate-program"
+              name='program_url'
+              label='Affiliate program URL'
+              placeholder='eg. jasper-ai/affiliate-program'
             />
             <TextInput
               control={methods.control}
-              name="description"
-              label="Short product description"
-              placeholder="eg. Content writing tool"
+              name='description'
+              label='Short product description'
+              placeholder='eg. Content writing tool'
             />
           </div>
           <div>
             <TextareaInput
               control={methods.control}
-              name="program_description"
-              label="Program description"
-              placeholder="eg. With jasper user can start creating massive blog, ebook, music , etc contents with AI."
-              className="h-40 text-cream-20"
+              name='program_description'
+              label='Program description'
+              placeholder='eg. With jasper user can start creating massive blog, ebook, music , etc contents with AI.'
+              className='h-40 text-cream-20'
             />
           </div>
-          <div className="flex gap-10 flex-col lg:flex-row">
+          <div className='flex gap-10 flex-col lg:flex-row'>
             <Dropdown
               control={methods.control}
-              name="affiliates"
-              label="Affiliate Type"
+              name='affiliates'
+              label='Affiliate Type'
               options={Affiliates}
-              placeholder="Affiliate Type"
+              placeholder='Affiliate Type'
             />
             <Dropdown
               control={methods.control}
-              name="levels"
-              label="Affiliates Levels"
+              name='levels'
+              label='Affiliates Levels'
               options={Levels}
-              placeholder="Affiliate Levels"
+              placeholder='Affiliate Levels'
             />
           </div>
-          <div className="flex gap-10 flex-col lg:flex-row">
+          <div className='flex gap-10 flex-col lg:flex-row'>
             <TextInput
               control={methods.control}
-              name="publisher_name"
-              label="Publisher Name"
-              placeholder="eg. John Don"
+              name='publisher_name'
+              label='Publisher Name'
+              placeholder='eg. John Don'
             />
             <TextInput
               control={methods.control}
-              name="publisher_email"
-              label="Publisher Email"
-              placeholder="eg. yourname@gmail.com"
+              name='publisher_email'
+              label='Publisher Email'
+              placeholder='eg. yourname@gmail.com'
             />
           </div>
-          <Button type="submit" className="w-full">
+          <Button type='submit' className='w-full'>
             Proceed
           </Button>
         </form>
       </FormProvider>
     </div>
-  );
-};
+  )
+}
 
-export default SubmitProgramForm;
+export default SubmitProgramForm
